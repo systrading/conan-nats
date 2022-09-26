653c9bfeb812c550087cf5884343b95660d86cf8
Josh B <josh@sys.trade>
2022-09-26 14:43:38 +1000

    Updated url, added streaming build option + cmake_find_package generator

--- conanfile.py
+++ conanfile.py
@@ -8,11 +8,12 @@ class NatsCConan(ConanFile):
     version = "2.1.0"
     description = "A C client for the NATS messaging system"
     topics = ("conan", "nats.c", "communication", "messaging", "protocols")
-    url = "https://github.com/mortensorensen/conan-nats.c"
+    url = "https://github.com/systrading/conan-nats"
     homepage = "https://github.com/nats-io/nats.c"
     license = "https://github.com/nats-io/nats.c/blob/master/LICENSE"
     exports_sources = ["CMakeLists.txt"]
-    generators = "cmake"
+    generators = "cmake", "cmake_find_package"
+    build_policy = "missing"
     settings = "os", "compiler", "build_type", "arch"
     options = {
         "shared": [True, False],
@@ -20,6 +21,7 @@ class NatsCConan(ConanFile):
         "tls": [True, False],
         "tls_force_host_verify": [True, False],
         "tls_use_openssl_1_1_api": [True, False],
+        "streaming": [True, False],
     }
     default_options = {
         "shared": False,
@@ -27,9 +29,9 @@ class NatsCConan(ConanFile):
         "tls": True,
         "tls_force_host_verify": True,
         "tls_use_openssl_1_1_api": True,
+        "streaming": False
     }
     _source_subfolder = "source_subfolder"
-    requires = "protobuf-c/1.3.2@mortensoerensen+conan-protobuf-c/stable"
 
     def source(self):
         sha256 = "1493ae3d790e2ebc4d77c65ef2957e2fb77182d69afeeeb2be1e1e6bee0ca12e"
@@ -41,9 +43,9 @@ class NatsCConan(ConanFile):
         cmake = CMake(self)
         cmake.definitions["NATS_BUILD_NO_SPIN"] = self.options.no_spin
         cmake.definitions["NATS_BUILD_WITH_TLS"] = self.options.tls
+        cmake.definitions["NATS_BUILD_STREAMING"] = self.options.streaming
         cmake.definitions["NATS_BUILD_TLS_FORCE_HOST_VERIFY"] = self.options.tls_force_host_verify
         cmake.definitions["NATS_BUILD_TLS_USE_OPENSSL_1_1_API"] = self.options.tls_use_openssl_1_1_api
-        cmake.definitions["CMAKE_MACOSX_RPATH"] = True
         cmake.configure()
         return cmake
 
@@ -52,7 +54,6 @@ class NatsCConan(ConanFile):
         cmake.build()
 
     def package(self):
-        self.copy(pattern="LICENSE", src=self._source_subfolder, dst="licenses")
         cmake = self._configure_cmake()
         cmake.install()
 
